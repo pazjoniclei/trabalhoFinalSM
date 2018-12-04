adda29328a599f61169ad254c6c40df07f1bb148
pazjoniclei <42686269+pazjoniclei@users.noreply.github.com>
2018-12-04 21:17:47 -0200

    trabalho pronto
    trabalho de dama modificado e fizemos as modificações no trabalho, tem como você colocar o nome do jogador, e tem como come duas pedras de vez e quando um ganhar mostra o nome de quem ganhou.

---
+++ montarTabuleiro.js
@@ -0,0 +1,143 @@
+$(function(){
+    var casa_selecionada = null;
+    var peca_selecionada = null;
+
+    var pontos_pretas = 0;
+    var pontos_brancas = 0;
+    function MontarTabuleiro(){
+    var i;
+
+    /*INICIO DO FOR PARA INCLUIR AS LINHAS*/
+            for (i=0; i<8; i++){
+                $("#tabuleiro").append("<div id='linha_"+i.toString()+"' class='linha' >");
+
+    /*INICIO DO FOR PARA INCLUIR AS CASAS*/
+                for (j=0; j<8; j++){
+                    var nome_casa ="casa_"+i.toString()+"_"+j.toString();
+                    var classe = (i%2==0?(j%2==0?"casa_branca":"casa_preta"):(j%2!=0?"casa_branca":"casa_preta"));
+                    $("#linha_"+i.toString()).append("<div id='"+nome_casa+"' class='casa "+classe+"' />");
+
+/*INICIO DA CONDIÇÃO IF PARA COLOCAR A PEDRA PRETA OU BRANCA*/
+                    if(classe == "casa_preta"){
+                        if(i < 3){
+                            $("#"+nome_casa).append("<img src='peca_preta.png' class='peca' id='"+nome_casa.replace("casa", "peca_preta")+"'/>");
+                        }
+                        else
+                        if(i > 4){
+                            $("#"+nome_casa).append("<img src='peca_branca.png' class='peca' id='"+nome_casa.replace("casa", "peca_branca")+"'/>");
+                        }
+
+                    }
+/*INICIO DA CONDIÇÃO IF PARA COLOCAR A PEDRA PRETA OU BRANCA*/
+                }
+  /*FIM DO FOR PARA INCLUIR AS CASAS*/
+            }
+    /*FIM DO FOR PARA INCLUIR AS LINHAS*/
+    }
+
+    MontarTabuleiro();
+
+/*INICIO DA FUNÇÃO DO CLIQUE*/
+$(".casa").click(function(){
+
+      //$("#blc").text($("#jogador1").val());
+    //  $("#bla").text($("#jogador2").val());
+      var aI, aJ, nI, nJ, dI, dJ;
+      var casa_anterior, peca_anterior;
+      if(peca_selecionada != null){
+          aI = parseInt(casa_selecionada.substr(5, 1));
+          aJ = parseInt(casa_selecionada.substr(7, 1));
+
+          nI = parseInt($(this).attr("id").substr(5, 1));
+          nJ = parseInt($(this).attr("id").substr(7, 1));
+
+          dI = parseInt(((aI - nI) < 0?(aI - nI)*(-1):(aI - nI)));
+          dJ = parseInt(((aJ - nJ) < 0?(aJ - nJ)*(-1):(aJ - nJ)));
+      }
+
+      $("#"+casa_selecionada).removeClass("casa_selecionada");
+      casa_anterior = casa_selecionada;
+      peca_anterior = peca_selecionada;
+      casa_selecionada = $(this).attr("id");
+      $("#"+casa_selecionada).addClass("casa_selecionada");
+      peca_selecionada = $("#"+casa_selecionada).children("img").attr("id");
+
+      if(peca_selecionada==null){
+          $("#info_peca_selecionada").text("NENHUMA PECA SELECIONADA");
+          if(peca_anterior != null){
+              if (((peca_anterior.indexOf("preta") >= 0) && (nI > aI)) ||   ((peca_anterior.indexOf("branca") >= 0) && (nI < aI))){
+                  var obj = $("#"+peca_anterior);
+                  if((dI == 1) && (dJ == 1)){                                 $("#"+casa_anterior).remove("#"+peca_anterior);
+                      $("#"+casa_selecionada).append(obj);
+                  }else
+                  if((dI == 2) && (dJ == 2)){
+                      if((peca_anterior.indexOf("branca") >= 0))   {
+                          var casa_meio = null, peca_meio = null;
+                          if(nJ < aJ){
+                              casa_meio = "#casa_"+(nI+1).toString()+"_"+(nJ+1).toString();
+                          }else{
+                              casa_meio = "#casa_"+(nI+1).toString()+"_"+(nJ-1).toString();
+                          }
+                          if($(casa_meio).children().size()>0)
+                          peca_meio = $(casa_meio).children("img");
+
+                          if(peca_meio != null){
+                              $("#compila").append(peca_meio);
+                              peca_meio.remove();
+                              pontos_brancas++;
+
+                              //inicio condição se a pontuação do jogador maior igual a 2 então acrescenta a palavra ponto no plural
+                              if(pontos_brancas >= 2){
+                                ponto = " Pontos";
+                              }else{
+                                ponto = " Ponto";
+                              }
+                              //fim condição se a pontuação do jogador maior igual a 2 então acrescenta a palavra ponto no plural
+
+                              $("#info_pontos_brancas").text($("#jogador2").val()+" "+pontos_brancas.toString()+ ponto);
+                              $("#"+casa_anterior).remove("#"+peca_anterior);
+                              $("#"+casa_selecionada).append(obj);
+                          }
+                      }else
+                      if((peca_anterior.indexOf("preta") >= 0))    {
+                          var casa_meio = null, peca_meio = null;
+                          if(nJ < aJ){
+                              casa_meio = "#casa_"+(nI-1).toString()+"_"+(nJ+1).toString();
+                          }else{
+                              casa_meio = "#casa_"+(nI-1).toString()+"_"+(nJ-1).toString();
+                          }
+                          if($(casa_meio).children().size()>0)
+                          peca_meio = $(casa_meio).children("img");
+                          if(peca_meio != null){
+                              peca_meio.remove();
+                              pontos_pretas++;
+                              //inicio condição se a pontuação do jogador maior igual a 2 então acrescenta a palavra ponto no plural
+                              if(pontos_pretas >= 2){
+                                ponto = " Pontos";
+                              }else{
+                                ponto = " Ponto";
+                              }
+                              //fim condição se a pontuação do jogador maior igual a 2 então acrescenta a palavra ponto no plural
+                              $("#info_pontos_pretas").text($("#jogador1").val()+" "+pontos_pretas.toString()+ ponto);
+                              $("#"+casa_anterior).remove("#"+peca_anterior);
+                              $("#"+casa_selecionada).append(obj);
+                          }
+                      }
+                  }
+              }
+          }
+      }
+      else{
+          $("#info_peca_selecionada").text(peca_selecionada.toString());
+      }
+
+
+      if(pontos_pretas == 12){
+          $("#info").html("<h1>"+"O JOGADOR "+$("#jogador1").val()+" GANHOU A PARTIDA"+"</h1>");
+      }else if (pontos_brancas == 12) {
+        $("#info").html("<h1>"+"O JOGADOR "+$("#jogador2").val()+" GANHOU A PARTIDA"+"</h1>");
+      }
+
+})
+
+});
